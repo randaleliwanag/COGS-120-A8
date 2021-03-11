c2d9a80d45cd2d4f9b2c729996ce84543ecc9eb9
Randale Liwanag <rliwanag@ucsd.edu>
2021-03-10 20:45:00 -0800

    Splash

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+const splash = document.querySelector(".splash");
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    setTimeout(()=>{
+        splash.classList.add("display-none");
+    }, 3000);
+})
+
 // Get today's date
 var date = new Date();
 
